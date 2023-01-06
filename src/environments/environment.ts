b09c87f2efbb651e2bd0da5131e43bbead8f646d
avto.kenchoshvili <tandila.92@gmail.com>
2023-01-06 15:57:27 +0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'portfolio-2e5d4',
+    appId: '1:607970865039:web:936917bac47bbada83a5fa',
+    databaseURL: 'https://portfolio-2e5d4-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'portfolio-2e5d4.appspot.com',
+    apiKey: 'AIzaSyCK3weCCcY3hDOWc3-evcxjQS3zynDh9k0',
+    authDomain: 'portfolio-2e5d4.firebaseapp.com',
+    messagingSenderId: '607970865039',
+    measurementId: 'G-KH0Q39KN00',
+  },
   production: false
 };
 
