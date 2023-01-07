02efe6d9d660f0817856066e07e539d6c62c951a
avto.kenchoshvili <tandila.92@gmail.com>
2023-01-08 03:52:33 +0400

    porfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,22 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './header/nav/nav.component';
+import { MainComponent } from './header/main/main.component';
+import { ContactComponent } from './header/contact/contact.component';
+import { SignInComponent } from './header/sign-in/sign-in.component';
+import { SignUpComponent } from './header/sign-up/sign-up.component';
+import { PortfolioComponent } from './header/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    ContactComponent,
+    SignInComponent,
+    SignUpComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
